7981c6c5ce116979132966ad1bd2f1bf4eedfd37
morganrivers <danielmorganrivers@gmail.com>
2024-09-09 00:48:08 +0200

    fixed some issues with dialogues to make them more like the books. Also, rubato seems very wrong, at first glance. But, chorusing works with arbitrary whales!

--- code/generate_whale_dialogue_txt.py
+++ code/generate_whale_dialogue_txt.py
@@ -1,8 +1,8 @@
 """
 This python script generates a dialogue using the augmented whale csv data. It does so by first labelling the
 whale by its number ("Whale" column) and the words that it says. When another whale speaks, the constructedText
-column is concatenated until that whale is interrupted. New conversations, defined by having a different "filename"
-are separated and labelled by their filename.
+column is concatenated until another whale speaks or the conversation ends. New conversations, defined by having
+a different "filename", are separated and labelled by their filename.
 """
 import pandas as pd
 
@@ -15,7 +15,7 @@ dialogues = []
 # Iterate over the rows in the dataframe
 for i, row in data.iterrows():
     # If it's the start of a new conversation, add a new dialogue
-    if i == 0 or row['File'] != data.loc[i - 1, 'File']:
+    if i == 0 or row['File'] != sorted_data.loc[i - 1, 'File']:
         dialogues.append({
             'file': row['File'],
             'dialogue': []
@@ -26,22 +26,6 @@ for i, row in data.iterrows():
         'whale': row['Whale'],
         'text': row['ConstructedString']
     })
-
-# # Print the dialogues
-# for dialogue in dialogues:
-#     print(f"File: {dialogue['file']}")
-#     previous_whale_name = "" # empty as there was no previous, it's the start of a conversation
-#     what_last_whale_said = ""
-#     for line in dialogue['dialogue']:
-#         if line['whale'] == previous_whale_name:
-#             what_last_whale_said = what_last_whale_said + " " + line['text']
-#         else:
-#             previous_whale_name = line["whale"]
-#             if not previous_whale_name == "" :
-#                 # print what the last whale said if not a new conversation
-#                 print(what_last_whale_said + ".") # end what it said with a period as it's the end of a sentence.
-#             # new whale is speaking
-#             what_last_whale_said = f"Whale {line['whale']}: {line['text']}"
 
 # Open the output file
 with open('../data/whale_dialogues.txt', 'w') as f:
